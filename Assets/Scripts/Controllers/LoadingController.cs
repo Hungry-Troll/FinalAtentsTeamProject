e36058b8976fe61e56008a2017a7b64e62203341
Hungry-Troll <i_am_gamer@naver.com>
2022-10-13 12:23:44 +0900

    <Feat>:<로딩창 변경>
    로딩바 변경, 로딩 이미지 랜덤으로 3개중 하나 나오게 변경

--- Assets/Scripts/Controllers/LoadingController.cs
+++ Assets/Scripts/Controllers/LoadingController.cs
@@ -8,16 +8,45 @@ public class LoadingController : MonoBehaviour
 {
     // 로딩창 컨트롤러
     [SerializeField]
+    // 로딩바
     Image _progressBar;
-    AsyncOperation _op; 
-    
+    // 로딩변수
+    AsyncOperation _op;
+    // 로딩창 컨트롤러
+    [SerializeField]
+    // 로딩 배경 변환용 이미지
+    Image _backImage;
+
     void Start()
     {
+        // 로딩 배경 변환용 코드
+        int tmpRandom = Random.Range(0, 3);
+        Sprite tmpSprite;
+        switch (tmpRandom)
+        {
+            case 0:
+                // 트리케라톱스 이미지
+                tmpSprite = GameManager.Resource.GetImage("BackImage1");
+                _backImage.sprite = tmpSprite;
+                break;
+            case 1:
+                // 파키케팔로 이미지
+                tmpSprite = GameManager.Resource.GetImage("BackImage2");
+                _backImage.sprite = tmpSprite;
+                break;
+            case 2:
+                // 브라키오 이미지
+                tmpSprite = GameManager.Resource.GetImage("BackImage3");
+                _backImage.sprite = tmpSprite;
+                break;
+        }
+        // 로딩창 열림
         StartCoroutine("LoadSceneProcess");
     }
 
     IEnumerator LoadSceneProcess()
     {
+
         float timer = 0f;
         _op = SceneManager.LoadSceneAsync(GameManager.Scene.LoadSceneName);
         _op.allowSceneActivation = false;
