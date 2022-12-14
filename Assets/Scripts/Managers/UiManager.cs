c9715d4051783b91dab985f7996b7fc055b2be27
NaeunYoon <whalebaby@naver.com>
2022-12-14 13:15:59 +0900

    <Feat>:<ui 물약사용 버그 수정>

--- Assets/Scripts/Managers/UiManager.cs
+++ Assets/Scripts/Managers/UiManager.cs
@@ -891,6 +891,7 @@ public class UiManager
                     int slotNum = 0;
                     slotNum = i;
                     GameManager.Ui._inventoryController._invenSlotList[slotNum].SetOverlapItemCntSub();
+                    GameManager.Ui._uiSceneAttackButton.PotionCountMinusOne();
                     // 만약 줄어든 뒤 갯수가 0개라면, 해당 인벤토리칸 지우기
                     if (GameManager.Ui._inventoryController._invenSlotList[slotNum]._invenItemCount == 0)
                     {
